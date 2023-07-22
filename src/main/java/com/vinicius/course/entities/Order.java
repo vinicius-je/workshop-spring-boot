f03ccd640710e2526f492432be5fa3cb191a7f80
vinicius-je <vinicius.je99@gmail.com>
2023-07-22 17:33:27 -0300

    feat: OrderStatus enum

--- src/main/java/com/vinicius/course/entities/Order.java
+++ src/main/java/com/vinicius/course/entities/Order.java
@@ -1,6 +1,7 @@
 package com.vinicius.course.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.vinicius.course.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -17,16 +18,20 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
 
+
     public Order() {}
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, User client, OrderStatus orderStatus) {
         this.id = id;
         this.moment = moment;
         this.client = client;
+        setOrderStatus(orderStatus);
     }
 
     public Long getId() {
@@ -51,6 +56,15 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null)
+            this.orderStatus = orderStatus.getCode();
     }
 
     @Override
