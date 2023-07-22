85bd8400317ac9fdcb3b14c3b4f0858aeb076f04
vinicius-je <vinicius.je99@gmail.com>
2023-07-22 17:15:04 -0300

    feat: Order, Instant and ISO 8601

---
+++ src/main/java/com/vinicius/course/resources/OrderResource.java
@@ -0,0 +1,30 @@
+package com.vinicius.course.resources;
+
+import com.vinicius.course.entities.Order;
+import com.vinicius.course.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/orders")
+public class OrderResource {
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping
+    public ResponseEntity<List<Order>> findAll() {
+        List<Order> orders = orderService.findAll();
+        return ResponseEntity.ok().body(orders);
+    }
+
+    @GetMapping(value = "/{id}")
+    public Order findById(@PathVariable Long id){
+        return orderService.findById(id);
+    }
+}
---
+++ src/main/java/com/vinicius/course/services/OrderService.java
@@ -0,0 +1,24 @@
+package com.vinicius.course.services;
+
+import com.vinicius.course.entities.Order;
+import com.vinicius.course.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderService {
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public List<Order> findAll(){
+        return orderRepository.findAll();
+    }
+
+    public Order findById(Long id){
+        Optional<Order> obj =  orderRepository.findById(id);
+        return obj.get();
+    }
+}
