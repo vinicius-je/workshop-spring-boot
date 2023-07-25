5d12dd46d4d35b3ccf43a1475cf5b7db7043ebbf
vinicius-je <vinicius.je99@gmail.com>
2023-07-25 16:25:42 -0300

    feat: user insert

--- src/main/java/com/vinicius/course/resources/UserResource.java
+++ src/main/java/com/vinicius/course/resources/UserResource.java
@@ -5,7 +5,9 @@ import com.vinicius.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -24,6 +26,14 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id) {
         User obj = userService.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj){
+        User user = userService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).body(user);
     }
 
 }
