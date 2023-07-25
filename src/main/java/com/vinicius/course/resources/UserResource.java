f7fb2aa6f3f31856a33a534aa1e23c26f0969286
vinicius-je <vinicius.je99@gmail.com>
2023-07-25 16:15:07 -0300

    refactor: userResource findById method

--- src/main/java/com/vinicius/course/resources/UserResource.java
+++ src/main/java/com/vinicius/course/resources/UserResource.java
@@ -4,10 +4,7 @@ import com.vinicius.course.entities.User;
 import com.vinicius.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,7 +21,9 @@ public class UserResource {
     }
 
     @GetMapping(value = "/{id}")
-    public User findById(@PathVariable Long id){
-        return userService.findById(id);
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = userService.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
+
 }
