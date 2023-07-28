477cf59afbadde585d6af52562ea7e0ecdb11e35
vinicius-je <vinicius.je99@gmail.com>
2023-07-27 22:09:57 -0300

    feat: order resource CRUD

--- src/main/java/com/vinicius/course/resources/OrderResource.java
+++ src/main/java/com/vinicius/course/resources/OrderResource.java
@@ -4,11 +4,10 @@ import com.vinicius.course.entities.Order;
 import com.vinicius.course.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -27,4 +26,26 @@ public class OrderResource {
     public Order findById(@PathVariable Long id){
         return orderService.findById(id);
     }
+
+    @PostMapping
+    public ResponseEntity<Order> insert(@RequestBody Order obj){
+        Order order = orderService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(order.getId()).toUri();
+        return ResponseEntity.created(uri).body(order);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id, @RequestBody Order obj){
+        Order order = orderService.update(id, obj);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Order> update(@PathVariable Long id, @RequestBody Order obj){
+        Order order = orderService.update(id, obj);
+        return ResponseEntity.ok().body(order);
+    }
+
+
 }
