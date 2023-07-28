59766edd2b96429011792fae55a28a624c79bbd4
vinicius-je <vinicius.je99@gmail.com>
2023-07-27 21:45:05 -0300

    feat: product resource insert, delete and update

--- src/main/java/com/vinicius/course/resources/ProductResource.java
+++ src/main/java/com/vinicius/course/resources/ProductResource.java
@@ -4,27 +4,46 @@ import com.vinicius.course.entities.Product;
 import com.vinicius.course.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductResource {
     @Autowired
-    private ProductService productServiceService;
+    private ProductService productService;
 
     @GetMapping
     public ResponseEntity<List<Product>> findAll() {
-        List<Product> products = productServiceService.findAll();
+        List<Product> products = productService.findAll();
         return ResponseEntity.ok().body(products);
     }
 
     @GetMapping(value = "/{id}")
     public Product findById(@PathVariable Long id){
-        return productServiceService.findById(id);
+        return productService.findById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> insert(@RequestBody Product obj){
+        Product product = productService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(product.getId()).toUri();
+        return ResponseEntity.created(uri).body(product);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        productService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping(value = "/{id}")
+    public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody Product obj){
+        Product product = productService.update(id, obj);
+        return ResponseEntity.ok().body(product);
     }
 }
