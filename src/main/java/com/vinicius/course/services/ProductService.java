1f5623c68d0aeeac7c573b95c05df3db3d183cc0
vinicius-je <vinicius.je99@gmail.com>
2023-07-27 21:44:35 -0300

    refactor: userService updateData

--- src/main/java/com/vinicius/course/services/ProductService.java
+++ src/main/java/com/vinicius/course/services/ProductService.java
@@ -51,7 +51,7 @@ public class ProductService {
         }
     }
 
-    public void updateData(Product entity, Product obj){
+    private void updateData(Product entity, Product obj){
         entity.setName(obj.getName());
         entity.setDescription(obj.getDescription());
         entity.setPrice(obj.getPrice());
