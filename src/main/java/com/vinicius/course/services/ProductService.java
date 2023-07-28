f39d767a06fe5cdf92c7f5a769da3a940b978dc6
vinicius-je <vinicius.je99@gmail.com>
2023-07-27 21:38:10 -0300

    feat: product CRUD service

--- src/main/java/com/vinicius/course/services/ProductService.java
+++ src/main/java/com/vinicius/course/services/ProductService.java
@@ -2,7 +2,12 @@ package com.vinicius.course.services;
 
 import com.vinicius.course.entities.Product;
 import com.vinicius.course.repositories.ProductRepository;
+import com.vinicius.course.services.exceptions.DatabaseException;
+import com.vinicius.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -22,6 +27,34 @@ public class ProductService {
         return obj.get();
     }
 
+    public Product insert(Product product){
+        return productRepository.save(product);
+    }
 
+    public void delete(Long id){
+        try {
+            productRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+    }
 
+    public Product update(Long id, Product obj){
+        try {
+            Product entity = productRepository.getReferenceById(id);
+            updateData(entity, obj);
+            return productRepository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    public void updateData(Product entity, Product obj){
+        entity.setName(obj.getName());
+        entity.setDescription(obj.getDescription());
+        entity.setPrice(obj.getPrice());
+        entity.setImgUrl(obj.getImgUrl());
+    }
 }
