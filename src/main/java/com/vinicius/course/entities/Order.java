bce0b126d667f246d1c5cac7715799d6de92e89c
vinicius-je <vinicius.je99@gmail.com>
2023-08-01 11:24:38 -0300

    feat: order validation

--- src/main/java/com/vinicius/course/entities/Order.java
+++ src/main/java/com/vinicius/course/entities/Order.java
@@ -3,6 +3,9 @@ package com.vinicius.course.entities;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.vinicius.course.entities.enums.OrderStatus;
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.io.Serializable;
 import java.time.Instant;
@@ -17,11 +20,12 @@ public class Order implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull(message = "Instant is mandatory")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
-
     private Integer orderStatus;
 
+    @NotNull(message = "Client is mandatory")
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
