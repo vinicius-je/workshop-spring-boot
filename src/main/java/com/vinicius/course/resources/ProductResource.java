ce7d7c46af93eaf24ccf7f24c0a29eadcef8ded0
vinicius-je <vinicius.je99@gmail.com>
2023-08-01 10:46:51 -0300

    feat: validation resource added

--- src/main/java/com/vinicius/course/resources/ProductResource.java
+++ src/main/java/com/vinicius/course/resources/ProductResource.java
@@ -2,6 +2,7 @@ package com.vinicius.course.resources;
 
 import com.vinicius.course.entities.Product;
 import com.vinicius.course.services.ProductService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
--- src/main/java/com/vinicius/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/vinicius/course/resources/exceptions/ResourceExceptionHandler.java
@@ -6,19 +6,12 @@ import com.vinicius.course.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
-import jakarta.validation.Path;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.Instant;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
