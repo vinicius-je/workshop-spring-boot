8eb2e71708532ecf63ed3136116d54ed37e98658
vinicius-je <vinicius.je99@gmail.com>
2023-08-02 13:33:25 -0300

    feat: category service insert, delete and update

--- src/main/java/com/vinicius/course/services/CategoryService.java
+++ src/main/java/com/vinicius/course/services/CategoryService.java
@@ -2,7 +2,11 @@ package com.vinicius.course.services;
 
 import com.vinicius.course.entities.Category;
 import com.vinicius.course.repositories.CategoryRepository;
+import com.vinicius.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -22,6 +26,27 @@ public class CategoryService {
         return obj.get();
     }
 
+    public Category insert(Category obj){
+        return categoryRepository.save(obj);
+    }
 
+    public void delete(Long id){
+        try {
+            categoryRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DataIntegrityViolationException(e.getMessage());
+        }
+    }
 
+    public Category update(Long id, Category obj){
+        try {
+            Category entity = categoryRepository.getReferenceById(id);
+            entity.setName(obj.getName());
+            return categoryRepository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+    }
 }
